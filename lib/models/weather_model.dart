efa42abe2d4b23132386318bb34ad11435999cdb
Murilo-Baron <muuhbarrow@gmail.com>
2024-08-21 12:03:41 -0300

    dinamicamente com a api

--- lib/models/weather_model.dart
+++ lib/models/weather_model.dart
@@ -2,11 +2,17 @@ class Weather {
   final String cityName;
   final double temperature;
   final String description;
+  final int humidity; // Adiciona a propriedade de umidade
+  final double windSpeed; // Adiciona a propriedade de velocidade do vento
+  final double feelsLike; // Adiciona a propriedade de sensação térmica
 
   Weather({
     required this.cityName,
     required this.temperature,
     required this.description,
+    required this.humidity,
+    required this.windSpeed,
+    required this.feelsLike,
   });
 
   factory Weather.fromJson(Map<String, dynamic> json) {
@@ -14,6 +20,9 @@ class Weather {
       cityName: json['name'],
       temperature: json['main']['temp'],
       description: json['weather'][0]['description'],
+      humidity: json['main']['humidity'], // Preenche a umidade
+      windSpeed: json['wind']['speed'], // Preenche a velocidade do vento
+      feelsLike: json['main']['feels_like'], // Preenche a sensação térmica
     );
   }
 }
--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -8,47 +8,209 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
-  final WeatherService _weatherService = WeatherService();
   Weather? _weather;
+  bool _isLoading = true;
 
   @override
   void initState() {
     super.initState();
-    _fetchWeather();
+    _fetchWeatherData();
   }
 
-  void _fetchWeather() async {
-    final weather = await _weatherService.fetchWeather('São Paulo');
+  Future<void> _fetchWeatherData() async {
+    WeatherService weatherService = WeatherService();
+    Weather weatherData = await weatherService.fetchWeather('São Paulo');
     setState(() {
-      _weather = weather;
+      _weather = weatherData;
+      _isLoading = false;
     });
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text('Clima')),
-      body: _weather == null
+      appBar: AppBar(
+        title: Text("Clima"),
+      ),
+      body: _isLoading
           ? Center(child: CircularProgressIndicator())
-          : Padding(
-              padding: const EdgeInsets.all(16.0),
-              child: Column(
-                children: [
-                  Text(
-                    _weather!.cityName,
-                    style: TextStyle(fontSize: 32, fontWeight: FontWeight.bold),
-                  ),
-                  Text(
-                    '${_weather!.temperature}°C',
-                    style: TextStyle(fontSize: 24),
-                  ),
-                  Text(
-                    _weather!.description,
-                    style: TextStyle(fontSize: 18),
-                  ),
-                ],
+          : SingleChildScrollView(
+              child: Padding(
+                padding: const EdgeInsets.all(16.0),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    _buildSearchBar(),
+                    SizedBox(height: 20),
+                    _buildCurrentWeather(),
+                    SizedBox(height: 20),
+                    _buildNextHoursForecast(),
+                    SizedBox(height: 20),
+                    _buildNextDaysForecast(),
+                    SizedBox(height: 20),
+                    _buildTodayDetails(),
+                  ],
+                ),
               ),
             ),
+    );
+  }
+
+  Widget _buildSearchBar() {
+    return TextField(
+      decoration: InputDecoration(
+        hintText: 'Procure por uma localização',
+        prefixIcon: Icon(Icons.search),
+        border: OutlineInputBorder(
+          borderRadius: BorderRadius.circular(12),
+        ),
+      ),
+      onSubmitted: (value) async {
+        setState(() {
+          _isLoading = true;
+        });
+        Weather weatherData = await WeatherService().fetchWeather(value);
+        setState(() {
+          _weather = weatherData;
+          _isLoading = false;
+        });
+      },
+    );
+  }
+
+  Widget _buildCurrentWeather() {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Text(
+          _weather?.cityName ?? '',
+          style: TextStyle(fontSize: 32, fontWeight: FontWeight.bold),
+        ),
+        Text(
+          '${_weather?.temperature.toStringAsFixed(2) ?? ''}°C',
+          style: TextStyle(fontSize: 48, fontWeight: FontWeight.w300),
+        ),
+        Text(
+          _weather?.description ?? '',
+          style: TextStyle(fontSize: 24, fontWeight: FontWeight.w300),
+        ),
+      ],
+    );
+  }
+
+  Widget _buildNextHoursForecast() {
+    // Por enquanto, este exemplo utiliza dados estáticos
+    // Você pode adaptar para usar os dados da API para previsão horária
+    return Container(
+      height: 100,
+      child: ListView.builder(
+        scrollDirection: Axis.horizontal,
+        itemCount: 8, // Supondo 8 horas de previsão
+        itemBuilder: (context, index) {
+          return _buildHourlyForecastItem();
+        },
+      ),
+    );
+  }
+
+  Widget _buildHourlyForecastItem() {
+    return Container(
+      width: 80,
+      margin: EdgeInsets.only(right: 8),
+      padding: EdgeInsets.all(8),
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(12),
+        color: Colors.grey[200],
+      ),
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Text("18:00", style: TextStyle(fontSize: 16)),
+          Icon(Icons.wb_sunny, size: 24),
+          Text("-3°C", style: TextStyle(fontSize: 16)),
+        ],
+      ),
+    );
+  }
+
+  Widget _buildNextDaysForecast() {
+    // Suponha que você tem uma lista de previsões diárias
+    return Container(
+      padding: EdgeInsets.all(16),
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(12),
+        color: Colors.grey[200],
+      ),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Text(
+            "Proximos 7 dias",
+            style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+          ),
+          SizedBox(height: 10),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              _buildDailyForecastItem(),
+              _buildDailyForecastItem(),
+              _buildDailyForecastItem(),
+              _buildDailyForecastItem(),
+              _buildDailyForecastItem(),
+            ],
+          ),
+        ],
+      ),
+    );
+  }
+
+  Widget _buildDailyForecastItem() {
+    return Column(
+      children: [
+        Text("Mon", style: TextStyle(fontSize: 16)),
+        Icon(Icons.wb_sunny, size: 24),
+        Text("-5°C", style: TextStyle(fontSize: 16)),
+      ],
+    );
+  }
+
+  Widget _buildTodayDetails() {
+    return Container(
+      padding: EdgeInsets.all(16),
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(12),
+        color: Colors.grey[200],
+      ),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Text(
+            "Detalhes de hoje",
+            style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+          ),
+          SizedBox(height: 10),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              _buildDetailItem("Humidade", "${_weather?.humidity ?? ''}%"),
+              _buildDetailItem("Vento", "${_weather?.windSpeed ?? ''} km/h"),
+              _buildDetailItem(
+                  "Temperatura", "${_weather?.feelsLike ?? ''}°C"),
+            ],
+          ),
+        ],
+      ),
+    );
+  }
+
+  Widget _buildDetailItem(String title, String value) {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Text(title, style: TextStyle(fontSize: 16)),
+        SizedBox(height: 5),
+        Text(value, style: TextStyle(fontSize: 16)),
+      ],
     );
   }
 }
