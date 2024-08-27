6aff6d9eade5924d8de88067b9f6da9803c54851
Murilo-Baron <muuhbarrow@gmail.com>
2024-08-27 11:59:56 -0300

    debug flag retirada

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/rendering.dart';
 import 'package:intl/date_symbol_data_local.dart'; // Para formatação de data
 import 'screens/home_screen.dart'; // Importa o arquivo onde a HomeScreen está definida
 
 void main() async {
+  debugPaintSizeEnabled = false; // Disable the debug banner
   WidgetsFlutterBinding.ensureInitialized();
   await initializeDateFormatting(); // Inicializa o formato de data
   runApp(const MyApp());
@@ -14,6 +16,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
